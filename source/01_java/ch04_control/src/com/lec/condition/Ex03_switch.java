701f90306f4017cec97664b174c9c8f5d36e418d
Thejiney <thejiney@gmail.com>
2024-10-04 18:04:39 +0900

    html tag

--- source/01_java/ch04_control/src/com/lec/condition/Ex03_switch.java
+++ source/01_java/ch04_control/src/com/lec/condition/Ex03_switch.java
@@ -2,23 +2,28 @@ package com.lec.condition;
 
 public class Ex03_switch {
 	public static void main(String[] args) {
-		int num=3;
+		int num = 3;
 		// switch(정수형이나 문자형이나 문자열만 가능)
-		switch(num) {
+		switch (num) {
 		case 1:
-			System.out.println("주사위 1"); break;
+			System.out.println("주사위 1");
+			break;
 		case 2:
-			System.out.println("주사위 2"); break;
+			System.out.println("주사위 2");
+			break;
 		case 3:
-			System.out.println("주사위 3"); break;
+			System.out.println("주사위 3");
+			break;
 		case 4:
-			System.out.println("주사위 4"); break;
+			System.out.println("주사위 4");
+			break;
 		case 5:
-			System.out.println("주사위 5"); break;
+			System.out.println("주사위 5");
+			break;
 		default:
 			System.out.println("주사위 6");
-		}//switch
-		
+		}// switch
+
 //		if(num==1) {
 //			System.out.println("주사위 1");
 //		}else if(num==2) {
@@ -32,8 +37,6 @@ public class Ex03_switch {
 //		}else {
 //			System.out.println("주사위 6");
 //		}//if
-		
-		
-		
-	}//main
-}//Class
+
+	}// main
+}// Class
