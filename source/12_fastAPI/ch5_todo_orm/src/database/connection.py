52cf80819beadef1beebed18ac85b7ebc6125631
Thejiney <thejiney@gmail.com>
2025-01-24 17:58:23 +0900

    orm

--- source/12_fastAPI/ch5_todo_orm/src/database/connection.py
+++ source/12_fastAPI/ch5_todo_orm/src/database/connection.py
@@ -6,8 +6,14 @@ DATABASE_URL = 'oracle+cx_oracle://scott:tiger@localhost:1521/?service_name=xe'
 
 # 엔진 생성
 engine = create_engine(DATABASE_URL, echo=True)
-
 # 세션 클래스 생성
 SessionFactory = sessionmaker(autocommit=False,
                               autoflush=False,
                               bind=engine)
+
+def get_db():
+  session = SessionFactory()
+  try:
+    yield session
+  finally:
+    session.close()
